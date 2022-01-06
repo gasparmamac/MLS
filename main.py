7ee385d73f55b5424e5fd2bcfdbcc9e6cd2acab9
gaspar mamac <gasparmamac@outlook.com>
2022-01-06 23:43:32 +0900

    set env at heroku for secret key

--- main.py
+++ main.py
@@ -27,8 +27,7 @@ import pandas as pd
 pd.options.display.float_format = '{:,.1f}'.format
 
 app = Flask(__name__)
-SECRET_KEY = os.urandom(32)
-app.config['SECRET_KEY'] = SECRET_KEY
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 Bootstrap(app)
 
 # Connect to database
